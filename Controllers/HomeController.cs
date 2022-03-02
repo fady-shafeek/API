8b098c83d2312faf1c6d75a8a49313b4009575d9
Fady Shafeek <Fady.Shafeek@hotmail.com>
2022-03-02 18:56:47 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskDay2.Controllers
+{
+    public class HomeController : Controller
+    {
+        HttpClient client = new HttpClient();
+
+        public ActionResult Index()
+        {
+            HttpResponseMessage Response = client.GetAsync("http://localhost:51375/api/company").Result;
+            string Message = Response.Content.ReadAsStringAsync().Result;
+            ViewBag.Message = JsonConvert.DeserializeObject<List<Employee>>(Message);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            HttpResponseMessage Response = client.GetAsync("http://localhost:51375/api/company").Result;
+            string Message = Response.Content.ReadAsStringAsync().Result;
+            List<Employee> Emps = JsonConvert.DeserializeObject<List<Employee>>(Message);
+            return View(Emps);
+        }
+
+        public ActionResult Contact()
+        {
+            Employee Dept = new Employee() { ID = 100, Name = "Sandra", Age = 22 };
+            string Data = JsonConvert.SerializeObject(Dept);
+            StringContent RequestBody = new StringContent(Data, Encoding.UTF8, "application/Json"); 
+            HttpResponseMessage Response = client.PostAsync("http://localhost:51375/api/company", RequestBody).Result;
+            if (Response.IsSuccessStatusCode)
+            {
+                ViewBag.Message = "Added";
+            }
+            else
+            {
+                ViewBag.Message = "Not Added";
+            }
+            return View();
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskDay2
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
